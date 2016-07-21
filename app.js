4fe1718d0f177a881533291e79282b8960ef66ae
段赫 <hevbevt@163.com>
2016-07-21 23:47:16 +0800

    CRUD BEAR

--- app.js
+++ app.js
@@ -73,8 +73,43 @@ router.route('/bears')
     .get(function (req, res) {
         Bear.find(function (err, bears) {
             if (err) {
-                res.send
+                res.send(err);
             }
+            res.json(bears);
+        })
+    });
+
+router.route('/bears/:bear_id')
+    .get(function (req, res) {
+        Bear.findById(req.params.bear_id, function (err, bear) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(bear);
+        })
+    })
+    .put(function (req, res) {
+        Bear.findById(req.params.bear_id, function (err, bear) {
+            if (err) {
+                res.send(err);
+            }
+            bear.name = req.body.name;
+            bear.save(function (err) {
+                if (err) {
+                    res.send(err);
+                }
+                res.json({ message: 'Bear updated!'});
+            })
+        })
+    })
+    .delete(function (req, res) {
+        Bear.remove({
+            _id: req.params.bear_id
+        }, function (err) {
+            if (err) {
+                res.send(err);
+            }
+            res.send({ message: 'Successfully deleted'});
         })
     })
 
