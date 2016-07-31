d5fe301d37d334319ee4fa2e3024162a9023fe93
段赫 <hevbevt@163.com>
2016-07-31 23:29:51 +0800

    gulp init

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+/**
+ * Created by duanhe on 16/7/30.
+ */
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    gutil = require('gulp-util'),
+    concat = require('gulp-concat'),
+    concatCss = require('gulp-concat-css'),
+    uglify = require('gulp-uglify');
+
+gulp.task('default',['watch']);
+
+gulp.task('jshint', function () {
+    return gulp.src('src/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('build-sass', function () {
+    return gulp.src('src/sass/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(concatCss('all.css'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('public/stylesheets'));
+});
+gulp.task('build-css', function () {
+    return gulp.src('src/css/**/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(concatCss('depCss.css'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('public/stylesheets'));
+});
+gulp.task('build-dep-js', function () {
+    return gulp.src(['src/js/lib/jquery.js',
+        'src/js/lib/angular.js',
+        'src/js/lib/angular-animate.js',
+        'src/js/lib/angular-touch.js',
+        'src/js/lib/ui-bootstrap.js',
+        'src/js/lib/angular-ui-router.js',
+        'src/js/lib/bootstrap.js'])
+        .pipe(gulp.dest('public/js/lib'));
+});
+gulp.task('build-js', function () {
+    return gulp.src('src/js/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(concat('bundle.js'))
+        .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('public/js'));
+});
+gulp.task('copyPartials', function() {
+    // copy any html files in source/ to public/
+    gulp.src('src/partials/*.html').pipe(gulp.dest('public/partials'));
+});
+
+gulp.task('build',['build-css', 'build-sass','build-dep-js', 'build-js', 'copyPartials']);
+
+gulp.task('watch', function () {
+    gulp.watch('src/js/**/*.js', ['jshint']);
+    gulp.watch('src/sass/**/*.scss', ['build-css']);
+});
