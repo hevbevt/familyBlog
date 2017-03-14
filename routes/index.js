a94539ac317c2aca0c3ec1acc5ba525487be78db
段赫 <hevbevt@163.com>
2017-03-14 14:41:41 +0800

    remove error handler

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 module.exports = function (app) {
     app.get('/', function (req, res) {
         res.render('index', {
-            title: '主页',
+            title: '博客主页',
             user: req.session.user,
             success: req.flash('success').toString(),
             error: req.flash('error').toString()
